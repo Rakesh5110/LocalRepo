16dcc34d353fbc9de2d471a44753074657d98f34
Rakesh <rakeshreddy2204@gmail.com>
2022-07-29 16:30:16 +0530

    this is commit from rakesh reddy

--- src/mavenFramework/Appium.java
+++ src/mavenFramework/Appium.java
@@ -9,12 +9,15 @@ public class Appium {
 		@Test
 		public void github() {
 			System.out.println("GitHub");
+			System.out.println("github2");
 		}
+		
   
 		
 		@Test
 		public void Testing() {
 			System.out.println("Testing");
+			System.out.println("Testing234");
 	}
 }
 
--- src/mavenFramework/RestApiTest.java
+++ src/mavenFramework/RestApiTest.java
@@ -7,6 +7,8 @@ public class RestApiTest {
 	public void postman() 
     {
 		System.out.println("Postman");
+		System.out.println("Testing.xml file ");
+		System.out.println("Postman tool usin API testing");
     	
 	}
     @Test 
